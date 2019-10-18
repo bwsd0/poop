21d267de908a52765aa2005a072b45fa19cff4f0
Barry Wasdell <barrywasdell@gmail.com>
2019-10-18 11:07:25 +0200

    Mitigate browser buffering preventing page render
    Browsers may buffer chunks instead of rendering the data as it arrives.
    This change attempts to fix this.
    
    Tested browsers:
    Google Chrome 77.0.3865.103
    Firefox for iOS 17.3
    Mozilla Firefox 60.9.0
    curl 7.64.0

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# poop
+poop sends an endless stream of HTTP chunk transfer encoded poop.
 
-The greatest thing ever.
+Its main use is a "hello world" sanity check or post-install test when
+updating versions of Go or Docker, for example.
 
-## Usage
-
+Usage:
+```sh
+go get [-u] github.com/bwasd/poop
+poop &
+$BROWSER http://localhost:8080
 ```
-go run main.go
-# now go to http://localhost:8080
-```
+
+In a terminal emulator that supports Unicode with a font that includes
+the poop emoji glyph, invoke `curl(1)` with the `-N` option to disable
+buffering.
--- main.go
+++ main.go
@@ -1,6 +1,19 @@
+// poop sends an endless stream of HTTP chunk transfer encoded poop.
+//
+// Usage:
+//
+//  go get [-u] github.com/bwasd/poop
+//  $BROWSER http://localhost:8080
+//
+// In a terminal emulator with Unicode support, with a font that
+// includes the poop emoji glyph, invoke `curl(1)` with the `-N` option
+// to disable buffering and enjoy this app from the comfort of your
+// terminal.
+
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"time"
 )
@@ -13,7 +26,26 @@ func makePoop(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "cannot make poop", http.StatusInternalServerError)
 		return
 	}
+
+	// Browsers may buffer chunks instead of rendering the data as it arrives;
+	// To mitigate this, write a partial response with a small payload (1KB) of
+	// non-renderable Unicode and prevent MIME-type sniffing which may also
+	// cause buffering.
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+
+	// Zero Width Space ("\u200B")
+	zws := []byte{0xE2, 0x80, 0x8B}
+	bufsz := 1024
+	buf := make([]byte, bufsz)
+	buf = bytes.Repeat(zws, bufsz/len(zws))
+	if _, err := w.Write(buf); err != nil {
+		panic(err)
+	}
+
 	for {
+		// FIXME: returning an indeterminate-length data stream can be used as an
+		// exploitable resource exhaustion vector.
 		select {
 		case <-req.Context().Done():
 			return
